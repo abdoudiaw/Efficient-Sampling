c1ee2c2557157be54cf0bdfcccbe10d11768144e
abdoudiaw <abdourahmane.diaw@polytechnique.edu>
2021-12-08 12:24:04 -0500

    added bash scripts to run tables

---
+++ test_tableI.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Specify the benchmark function and sampler for the test
+export Function='easom'           # options: easom, hartmann6, michal2, rast, rosen, rosen8
+export Sampler_DIR='looseopt'          # options: looseopt, loosernd, tightopt,tightrnd
+#
+#
+##### DO NOT MODIFY THESE LINES #####
+export Table_DIR=/Users/diaw/Efficient-Sampling/tableI
+export Results_DIR=/Users/diaw/Efficient-Sampling/results/tableI
+#
+cd ${Results_DIR}
+mkdir "${Function}"
+cd    "${Function}"
+mkdir "${Sampler_DIR}"
+cd    "${Sampler_DIR}"
+#
+cp ${Table_DIR}/${Function}/${Sampler_DIR}/* .
+#
+# run test
+echo  "Running ${Function} with sampler ${Sampler_DIR}"
+
+python ${Results_DIR}/${Function}/${Sampler_DIR}/main_workflow.py
+#
+echo "Finished test table II. Cleaning up"
+#
+cd ${Table_DIR}/..
---
+++ test_tableII.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Specify the benchmark function and sampler for the test
+export Function='ackley'   # options: ackley, branins, hartmann6, michal5, rosen3, rosen8
+export Sampler_DIR='fmin'          # options: fmin, powell
+#
+#
+##### DO NOT MODIFY THESE LINES #####
+export Table_DIR=/Users/diaw/Efficient-Sampling/tableII
+export Results_DIR=/Users/diaw/Efficient-Sampling/results/tableII
+#
+cd ${Results_DIR}
+mkdir "${Function}"
+cd    "${Function}"
+mkdir "${Sampler_DIR}"
+cd    "${Sampler_DIR}"
+#
+cp ${Table_DIR}/${Function}/${Sampler_DIR}/* .
+#
+# run test
+
+echo  "Running ${Function} with sampler ${Sampler_DIR}"
+
+python ${Results_DIR}/${Function}/${Sampler_DIR}/main_workflow.py
+#
+echo "Finished test table I. Cleaning up"
+#
+cd ${Table_DIR}/..
